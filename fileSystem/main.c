db966f0f063f74d51516d647d283d27c0c868bde
Codingwal <florian.herzog08@gmail.com>
2025-01-11 11:43:22 +0100

    feature: bigger test

--- fileSystem/main.c
+++ fileSystem/main.c
@@ -5,16 +5,32 @@
 
 int main()
 {
+    // Initialize fileSystem
     FileSystem *fileSystem = malloc(sizeof(FileSystem));
     fileSystem_init(fileSystem);
 
-    FileInfo *file = fileSystem_getFileInfo(fileSystem, "bin");
+    // Create "user/documents"
+    FileInfo *documents = fileSystem_createFileInfo(fileSystem, "user/documents");
+    if (!documents)
+        printf("Failed to create user/documents\n");
+
+    // Create "user/documents/test.txt"
+    FileInfo *test = fileSystem_createFileInfo(fileSystem, "user/documents/test.txt");
+    if (!test)
+        printf("Failed to create user/documents/test.txt\n");
+
+    // Create "user/documents/test.txt"
+    if (fileSystem_deleteFileInfo(fileSystem, "user/documents/test.txt", false) == FAILURE)
+        printf("Failed to delete user/documents/test.txt\n");
+
+    // Get "user/documents/test.txt"
+    FileInfo *file = fileSystem_getFileInfo(fileSystem, "user/documents");
     if (file)
     {
-        printf("%s: size=%d\n", file->name, file->size);
+        fileSystem_printFileInfo(file);
     }
     else
-        printf("Failed to get file info");
-
-    return SUCCESS;
+        printf("Failed to get file info\n");
+    
+    fileSystem_printFileInfo(fileSystem_getFileInfo(fileSystem, "user/documents/test.txt"));
 }
